b0e5f77a51f3b16ceca2212efe9a6e6c341ed2e0
Oliver Bestmann <obest@esailors.de>
2023-11-12 19:44:25 +0100

    feat: show a progressbar when loading images.

--- frontend/src/app/components/image-view/image-view.component.ts
+++ frontend/src/app/components/image-view/image-view.component.ts
@@ -1,24 +1,47 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, signal} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import type {MediaItem} from '../../service/gallery';
-import {BehaviorSubject, distinctUntilChanged} from 'rxjs';
+import {ProgressBarComponent} from '../progressbar/progress-bar.component';
+import {BehaviorSubject, concatWith, distinctUntilChanged, filter, map, startWith, switchMap, timer} from 'rxjs';
+import {toObservable} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-image-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProgressBarComponent],
   templateUrl: './image-view.component.html',
   styleUrls: ['./image-view.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ImageViewComponent {
-  private readonly visibleSubject = new BehaviorSubject(false);
-  protected readonly visible$ = this.visibleSubject.pipe(distinctUntilChanged());
+  protected readonly visibleSignal = signal(false);
+
+  private readonly loadedSubject = new BehaviorSubject(false);
+
+  protected readonly loaderIsVisible$ = toObservable(this.visibleSignal).pipe(
+    distinctUntilChanged(),
+    filter(visible => visible),
+    switchMap(value => {
+      const notYetLoaded$ = this.loadedSubject.pipe(map(loaded => !loaded));
+
+      return timer(250).pipe(
+        startWith(false),
+        concatWith(notYetLoaded$),
+      )
+    })
+  )
 
   @Input({required: true})
   public media!: MediaItem;
 
   public visible(visible: boolean) {
-    this.visibleSubject.next(visible);
+    if (this.visibleSignal() != visible) {
+      this.loadedSubject.next(false);
+      this.visibleSignal.set(visible);
+    }
+  }
+
+  protected onImageLoad() {
+    this.loadedSubject.next(true);
   }
 }
---
+++ frontend/src/app/components/progressbar/progress-bar.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-progress-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './progress-bar.component.html',
+  styleUrl: './progress-bar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProgressBarComponent {
+}
